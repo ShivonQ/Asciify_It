a8d80872882ea7b259c626b9c63957552cb5b15f
ShivonQ <realitykeeper@gmail.com>
2016-02-09 14:23:37 -0600

    my ascii art thing

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(name='AsciiArt',
+      version='0.2.dev',
+      description='Ascii "art" from images',
+      author='malcolm',packages=['asciiart'],
+      install_requires=['pillow'])
